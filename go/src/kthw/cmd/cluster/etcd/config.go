0f0b7c46f801bd1f1661aa8f2641ff8cae2b1180
Timo Hirt <timo.hirt@moneyfarm.com>
2019-03-13 21:23:31 +0100

    etcd SSL setup

--- go/src/kthw/cmd/cluster/etcd/config.go
+++ go/src/kthw/cmd/cluster/etcd/config.go
@@ -24,13 +24,21 @@ StartLimitInterval=0
 ExecStart=/usr/local/bin/etcd \
   --name {{.NodeName}} \
   --data-dir /var/lib/etcd \
-  --listen-client-urls "http://{{.PrivateIP}}:2379,http://localhost:2379" \
-  --advertise-client-urls "http://{{.PrivateIP}}:2379" \
-  --initial-cluster "{{.NodeName}}=http://localhost:2380" \
+  --listen-client-urls "https://{{.PrivateIP}}:2379,https://localhost:2379" \
+  --advertise-client-urls "https://{{.PrivateIP}}:2379" \
+  --initial-cluster "{{.NodeName}}=https://{{.PrivateIP}}:2380" \
+  --initial-advertise-peer-urls "https://{{.PrivateIP}}:2380" \
+  --listen-peer-urls "https://{{.PrivateIP}}:2380" \
   --heartbeat-interval 200 \
   --election-timeout 5000 \
+  --trusted-ca-file /etc/kubernetes/pki/ca.pem \
   --cert-file /etc/kubernetes/pki/etcd.pem \
-  --key-file /etc/kubernetes/pki/etcd-key.pem
+  --key-file /etc/kubernetes/pki/etcd-key.pem \
+  --client-cert-auth \
+  --peer-trusted-ca-file /etc/kubernetes/pki/ca.pem \
+  --peer-cert-file /etc/kubernetes/pki/etcd.pem \
+  --peer-key-file /etc/kubernetes/pki/etcd-key.pem \
+  --peer-client-cert-auth
 
 [Install]
 WantedBy=multi-user.target
